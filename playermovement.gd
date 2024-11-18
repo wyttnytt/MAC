90931d139a11c05be47ef20112f60201aae999c0
onebigmeal <hyperpheonixreal@gmail.com>
2024-11-18 18:26:12 +0800

    crouch (sus but not as sus as red (before))(sus)(but like not too much)(like just the right amount of susocity)

--- playermovement.gd
+++ playermovement.gd
@@ -1,16 +1,17 @@
 extends RigidBody3D
-
+const CROUCH_SPEED = 1.0
 const WALK_SPEED = 75.0
 const MAX_WALK_SPEED = 15.0
 const AIR_SPEED = 10.0
 const SPRINT_SPEED = 80.0
-const SENSITIVITY = 0.01
+const SENSITIVITY = 0.004
 const BOB_FREQ = 2.0
 const BOB_AMP = 0.0
 const JUMP_HEIGHT = 10.0
 var t_bob = 0.0
 const BASE_FOV = 75.0
 const FOV_CHANGE = 1
+var friction = 0
 var was_on_floor 
 var direction = Vector3()
 var velocity = Vector3()
@@ -30,7 +31,7 @@ func _ready() -> void:
 	self.set_contact_monitor(true)
 	self.set_max_contacts_reported(999)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	linear_damp = 1
+	linear_damp = friction
 	raycast.enabled = true
 	front.enabled = true
 	left.enabled = true
@@ -57,7 +58,6 @@ func _touching_wall(vector) -> Vector3:
 	back.force_raycast_update() 
 	left.force_raycast_update() 
 	right.force_raycast_update()
-	print(front.is_colliding())
 	if front.is_colliding():
 		vector.z = clamp(vector.z,0,999)
 	if back.is_colliding():
@@ -82,6 +82,23 @@ func _process(delta: float) -> void:
 		if not is_on_floor:
 			apply_central_impulse(input*AIR_SPEED*delta)
 		else:
-			apply_central_impulse(input*WALK_SPEED*delta)
+			if Input.is_action_just_pressed("crouch"):
+				apply_central_impulse(input*CROUCH_SPEED*delta)
+			else:
+				apply_central_impulse(input*WALK_SPEED*delta)			
 	else: 
 		pass
+	#crouching below
+	if Input.is_action_just_pressed("crouch"):
+		$"../AnimationPlayer".play("crouch")
+		print ("whar")
+	elif Input.is_action_just_released("crouch"):
+		$"../AnimationPlayer".play_backwards("crouch")
+
+func _on_rigid_body_3d_body_entered(body: Node) -> void:
+	$"../AnimationPlayer".pause("crouch")
+	print ("amongus")
+
+func _on_rigid_body_3d_body_exited(body: Node) -> void:
+	$"../AnimationPlayer".resume("crouch")
+	print ("sus")
